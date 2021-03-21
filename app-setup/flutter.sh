ff74f1cf6b7b9d0e7d5656692af389a5e5787a94
leonardov31 <leonardoviana098@gmail.com>
2021-03-21 10:12:36 -0300

    Cloning just the stable version of flutter

--- app-setup/flutter.sh
+++ app-setup/flutter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 echo 'Cloning Flutter'
-git clone https://github.com/flutter/flutter.git ~/.dev/flutter
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 ~/.dev/flutter
 
 echo 'Adding flutter to path'
 set -Ua fish_user_paths $HOME/.dev/flutter/bin
